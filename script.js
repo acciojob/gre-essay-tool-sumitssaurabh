09711c5922f73e0e050ebd3da932cd3fecc87734
sumitssaurabh <117895961+sumitssaurabh@users.noreply.github.com>
2023-01-30 00:00:12 +0530

    1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+
+// function countChar(,){
+// 	var length = document.getElementById(evaluatedTExt).value.length;
+// 	document.getElementById(wordCount).innerHTML = length;
+// }
+
+
+function countChar(evaluatedTExt,wordCount){
+	var length = document.getElementById(evaluatedTExt).value.length;
+	document.getElementById(wordCount).innerHTML = length;
+}
